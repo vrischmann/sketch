4936de3834a2c70db563d19975c072bb42d953cc
Josh Bleecher Snyder <josharian@gmail.com>
2025-05-07 13:50:04 +0000

    all: make update_tests.sh scripts location-independent
    Co-Authored-By: sketch <hello@sketch.dev>

--- dockerimg/update_tests.sh
+++ dockerimg/update_tests.sh
@@ -3,9 +3,8 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 CURRENT_DIR=$(pwd)
 
-if [ "$SCRIPT_DIR" != "$CURRENT_DIR" ]; then
-    echo "Error: This script must be run from its own directory: $SCRIPT_DIR" >&2
-    exit 1
-fi
+cd "$SCRIPT_DIR"
 
 go test -httprecord ".*" -rewritewant
+
+cd "$CURRENT_DIR"
--- loop/update_tests.sh
+++ loop/update_tests.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+CURRENT_DIR=$(pwd)
+
+cd "$SCRIPT_DIR"
+
 go test -httprecord .
 go test
+
+cd "$CURRENT_DIR"
